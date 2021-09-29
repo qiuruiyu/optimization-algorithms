e2c70d6abdf618642488d371b2ac04ad0867870a
qiuruiyu <359603247@qq.com>
2021-09-29 21:30:10 +0800

    commit

---
+++ steepestDescentMethod.m
@@ -0,0 +1,37 @@
+clear;clc;cla;
+syms x1 x2 t
+% only for 2 dimensional condition
+f(x1, x2) = (x1-x2)^2 + (x1-1)^2;
+dfx1 = diff(f, x1);
+dfx2 = diff(f, x2);
+
+x = linspace(-2, 2);
+y = linspace(-2, 2);
+[X1, X2] = meshgrid(x, y);
+Z = (X1-X2).^2 + (X1-1).^2;
+figure("The path of x");
+contour(X1, X2, Z);
+axis equal;
+hold on;
+
+e = 1e-3;
+iters = 0;
+x0 = [0, 0];
+x_record = x0;
+df = [dfx1(x0(1), x0(2)), dfx2(x0(1), x0(2))];
+while norm(df) > e
+    iters = iters + 1;
+    xk = x_record(end, :);
+    % get best direction 
+    dk = -[dfx1(xk(1), xk(2)), dfx2(xk(1), xk(2))];
+    dk = dk / norm(dk);
+    % get best step length
+    g(x1, x2, t) = f(x1+dk(1)*t, x2+dk(2)*t);
+    dgt = diff(g, t);
+    tk = solve(dgt(xk(1), xk(2), t));
+    % update the x position 
+    xk1 = xk + tk * dk;
+    x_record = [x_record; xk1];
+    plot([xk(1), xk1(1)], [xk(2), xk1(2)]);
+    df = [dfx1(xk1(1), xk1(2)), dfx2(xk1(1), xk1(2))];
+end
