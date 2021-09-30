149a0df10c03dde55464b55ca5e4d9d5790e36ed
qiuruiyu <359603247@qq.com>
2021-09-30 13:56:50 +0800

    commit

--- steepestDescentMethod.m
+++ steepestDescentMethod.m
@@ -5,19 +5,23 @@ f(x1, x2) = (x1-x2)^2 + (x1-1)^2;
 dfx1 = diff(f, x1);
 dfx2 = diff(f, x2);
 
-x = linspace(-2, 2);
-y = linspace(-2, 2);
+x = linspace(-1, 1);
+y = linspace(-1, 1);
 [X1, X2] = meshgrid(x, y);
 Z = (X1-X2).^2 + (X1-1).^2;
-figure("The path of x");
+figure(1);
 contour(X1, X2, Z);
-axis equal;
+axis equal;  
 hold on;
+
+% timing start
+tic;
 
 e = 1e-3;
 iters = 0;
 x0 = [0, 0];
 x_record = x0;
+scatter(x0(1), x0(2), 'b');
 df = [dfx1(x0(1), x0(2)), dfx2(x0(1), x0(2))];
 while norm(df) > e
     iters = iters + 1;
@@ -26,12 +30,25 @@ while norm(df) > e
     dk = -[dfx1(xk(1), xk(2)), dfx2(xk(1), xk(2))];
     dk = dk / norm(dk);
     % get best step length
-    g(x1, x2, t) = f(x1+dk(1)*t, x2+dk(2)*t);
+    g(x1, x2, t) = f(x1+dk(1)*t, x2+dk(2)*t);  
     dgt = diff(g, t);
     tk = solve(dgt(xk(1), xk(2), t));
     % update the x position 
     xk1 = xk + tk * dk;
+    % modify the step to avoid vertical condition 
+%     xk1 = xk + 0.9 * tk * dk;
     x_record = [x_record; xk1];
-    plot([xk(1), xk1(1)], [xk(2), xk1(2)]);
+    scatter(xk1(1), xk1(2), 'b');
+    plot([xk(1), xk1(1)], [xk(2), xk1(2)], 'r', 'LineWidth', 2);
     df = [dfx1(xk1(1), xk1(2)), dfx2(xk1(1), xk1(2))];
 end
+
+% timing end 
+toc;
+
+disp("Best solution found.");
+disp("x = [" + double(xk1(1)) + ", " + double(xk1(2)) + "]");
+disp("Iteration: " + iters);
+disp("Running Time: " + toc);
+
+
